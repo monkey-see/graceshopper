cfa3f00eb28777f303eb5d604f05b305e5e22b18
MarcosRS <MarcosRS16@gmail.com>
2017-04-25 15:31:51 -0400

    add: cart styling

---
+++ app/components/CartContainer.jsx
@@ -0,0 +1,140 @@
+import React from 'react'
+import {Link} from 'react-router'
+import {connect} from 'react-redux'
+import {updateOrderInDB} from '../reducers/orders'
+
+function CartContainer(props) {
+  if( props.order.glasses && props.order.glasses.length > 0) {
+
+    return (
+
+      <div>
+       <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-10 col-md-offset-1" style={{color:'#c12e2a'}}>
+            <h1>Your Shopping Cart</h1>
+          </div>
+        </div>
+      </div>
+      <div className="container" >
+      <div className="row" >
+          <div className="col-sm-12 col-md-10 col-md-offset-1" style={{border:'1px solid #ccc',borderRadius: '10px', paddingTop: '10px'}}>
+              <table className="table table-hover">
+                  <thead>
+                      <tr>
+                          <th>Product</th>
+                          <th>Quantity</th>
+                          <th className="text-center">Price</th>
+                          <th className="text-center">Remove</th>
+                          
+                      </tr>
+                  </thead>
+                  <tbody>
+                      { props.order.glasses && props.order.glasses.map(glasses => {
+                          return (
+
+                        <tr>
+                          <td key={glasses.id} className="col-md-8" style={{verticalAlign: 'middle',fontFamily: 'Raleway, sans-serif'}} >
+                          <div className="media">
+                              <Link to={`/glasses/${glasses.id}`} className=" pull-left" style={{marginRight: '10px'}}>
+                                <img className="media-object img-responsive" src={glasses.image}  style={{width: '200px', height: '70px'}}/> 
+                              </Link>
+                              <div className="media-body" >
+                                  <h4 className="media-heading"><Link to={`/glasses/${glasses.id}`}>{glasses.name}</Link></h4>
+                                  <span>Status: </span><span className="text-danger"><strong>Leaves warehouse in 2 - 3 weeks</strong></span>
+                              </div>
+                          </div>
+                          </td>
+                          <td className="col-md-1" style={{textAlign: 'center', verticalAlign: 'middle'}}>
+                          <strong>1</strong>
+                          </td>
+                          <td className="col-md-1 text-center"  style={{textAlign: 'center', verticalAlign: 'middle'}}><strong>${glasses.price / 100}</strong></td>
+                          
+                          <td className="col-md-1"  style={{textAlign: 'center', verticalAlign: 'middle'}}>
+                            <button className="btn btn-danger" onClick={() => {
+                                const newGlasses = []
+                                props.order.glasses.forEach(singleGlasses => {
+                                  if (glasses.id !== singleGlasses.id) newGlasses.push(singleGlasses.id)
+                                })
+                                props.removeGlassesFromOrder(props.order.id, newGlasses)
+                              }} >Delete</button>
+
+                           </td>
+                        </tr>
+                          )
+                        })
+                      }
+
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td style={{color:'#c12e2a'}}><h5>Subtotal:</h5></td>
+                          <td className="text-center">
+                            <h5><strong>
+                                  ${props.order.glasses && props.order.glasses.reduce((totalPrice, glasses) => {
+                                    return totalPrice + glasses.price / 100
+                                  }, 0)}
+                                </strong>
+                            </h5>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td><h5 style={{color:'#c12e2a'}}>Shipping:</h5></td>
+                          <td className="text-center"><h5><strong>$10.94</strong></h5></td>
+                      </tr>
+                      <tr>
+                          <td></td>
+                          <td></td>
+                          <td><h3 style={{color:'#c12e2a'}}>Total</h3></td>
+                          <td className="text-center"><h3><strong>${  props.order.glasses && props.order.glasses.reduce((totalPrice, glasses) => {
+                                                                      return totalPrice + glasses.price / 100 
+                                                                   }, 10.94)} </strong></h3></td>
+                      </tr>
+                       <tr>
+                          <td>
+                            <Link to='/'><button className="btn btn-primary pull-left">Continue Shopping</button></Link>
+                          </td>
+                          <td></td>
+                          <td></td>
+                          <td>
+                            <Link to='/checkout'><button className="btn btn-success">Checkout</button></Link>
+                         </td>
+                      </tr>                                        
+                  </tbody>
+              </table>
+            </div>
+        </div>
+      </div>
+    </div>)
+    }else{
+      return (
+      <div className="container text-center">
+        <div className="row" >
+          <div className="col-sm-12" style={{color:'#c12e2a',marginBottom:'20px' }}>
+            <h1>Your shopping cart is currently empty</h1>
+          </div>
+          <div className="col-sm-12" style={{color:'#c12e2a'}}>
+            <Link to='/'><button className="btn btn-primary ">Continue Shopping</button></Link>
+          </div>
+        </div>
+      </div>
+      )
+    }
+}
+
+export default connect(
+  function mapStateToProps(state) {
+    return {
+      order: state.order
+    }
+  },
+  function mapDispatchToProps(dispatch) {
+    return {
+      removeGlassesFromOrder: (orderId, glassesIdArr) => {
+        dispatch(updateOrderInDB(orderId, glassesIdArr))
+      }
+    }
+  }
+)(CartContainer)
---
+++ app/components/CheckoutContainer.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {processOrder} from 'APP/app/reducers/orders'
+import {browserHistory} from 'react-router'
+
+function CheckoutContainer(props) {
+  function completeOrder(event) {
+    event.preventDefault()
+    props.processOrder(props.order.id, 'processing')
+    browserHistory.push('/success')
+  }
+  return (
+    // is it possible to call totalPrice getterMethod?
+    <div>
+      <h1>Checkout</h1>
+      <ul>
+      {
+        props.order.glasses && props.order.glasses.map(glasses => {
+          return (
+            <li key={glasses.id} className="list-group-item">{glasses.name}: ${glasses.price / 100}</li>
+          )
+        })
+      }
+      <li className="list-group-item">Total Price: ${props.order.glasses && props.order.glasses.reduce((totalPrice, glasses) => {
+        return totalPrice + glasses.price / 100
+      }, 10.94)}</li>
+      </ul>
+      <hr />
+      <form className="form-group col-lg-3" onSubmit={completeOrder}>
+        <h5>Personal Info</h5>
+        <input className="form-control" placeholder="First Name" name="first-name" />
+        <input className="form-control" placeholder="Last Name" name="last-name" />
+        <input className="form-control" placeholder="Phone Number" name="phone-number" />
+        <h5>Shipping Address</h5>
+        <input className="form-control" placeholder="Address Line 1" name="address-line-1" />
+        <input className="form-control" placeholder="Address Line 2" name="address-line-2" />
+        <input className="form-control" placeholder="City" name="city" />
+        <input className="form-control" placeholder="State" name="state" />
+        <input className="form-control" placeholder="ZIP Code" name="zip-code" />
+        <h5>Billing Address</h5>
+        <input className="form-control" placeholder="Address Line 1" name="address-line-1" />
+        <input className="form-control" placeholder="Address Line 2" name="address-line-2" />
+        <input className="form-control" placeholder="City" name="city" />
+        <input className="form-control" placeholder="State" name="state" />
+        <input className="form-control" placeholder="ZIP Code" name="zip-code" />
+        <h5>Credit Card Info</h5>
+        <input className="form-control" placeholder="Credit Card Number" name="credit-card-number" />
+        <input className="form-control" placeholder="CCV" name="ccv" />
+        <hr />
+        <input className="btn btn-primary" type="submit" value="Pay Now" />
+      </form>
+    </div>
+  )
+}
+
+export default connect(
+  function mapStateToProps(state) {
+    return {
+      order: state.order
+    }
+  },
+  function mapDispatchToProps(dispatch) {
+    return {
+      processOrder: (orderId, status) => {
+        dispatch(processOrder(orderId, status))
+      }
+    }
+  }
+)(CheckoutContainer)
