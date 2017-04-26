844a0a6aaaa5b31eb125f0659124a7b8572f73fa
Daniel Fattal <dffattal@gmail.com>
2017-04-25 21:06:47 -0400

    add extension 404 middleware

--- server/start.js
+++ server/start.js
@@ -54,16 +54,16 @@ module.exports = app
   // Serve our api - ./api also requires in ../db, which syncs with our database
   .use('/api', require('./api'))
 
-  // // any requests with an extension (.js, .css, etc.) turn into 404
-  // .use((req, res, next) => {
-  //   if (path.extname(req.path).length) {
-  //     const err = new Error('Not found')
-  //     err.status = 404
-  //     next(err)
-  //   } else {
-  //     next()
-  //   }
-  // })
+  // any requests with an extension (.js, .css, etc.) turn into 404
+  .use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
 
   // Send index.html for anything else.
   .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
